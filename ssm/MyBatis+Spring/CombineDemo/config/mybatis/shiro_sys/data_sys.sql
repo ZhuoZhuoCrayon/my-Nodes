1ca8dc2cd03a0a13cd8607ec0a5cd6246171d52c
caixiaoxin <873217631@qq.com>
2019-11-27 21:30:04 +0800

    修复数据库插值文件：添加.do后缀

--- ssm/MyBatis+Spring/CombineDemo/config/mybatis/shiro_sys/data_sys.sql
+++ ssm/MyBatis+Spring/CombineDemo/config/mybatis/shiro_sys/data_sys.sql
@@ -1,6 +1,6 @@
 use company_sys;
-insert into sys_users values(1,'admin','95657d3e3052fb39d70d610c70a9a575','87cc486c53b49f72f5b96bb55d93bc7f');
-insert into sys_users values(2,'tycoding','7de3848c92d39e98f7c74139f1a079d7','6478ccf88032592fe9396f008408400b');
+insert into sys_users values(1,'admin','3bb0b0e9f079312bb881bce0d0346e52','315a6b25cc2d8a5af98f5768e2945e7a');
+insert into sys_users values(2,'user','fa2a26aa8be21a5ec4fae415be6ac657','8fb10b18001d1e492130bbdc2285a032');
 
 insert into sys_roles values(1,'admin', '管理员');
 insert into sys_roles values(2,'user','普通用户');
@@ -9,41 +9,41 @@ insert into sys_roles values(2,'user','普通用户');
 insert into sys_users_roles values(1,1, 1);
 insert into sys_users_roles values(2,2, 2);
 
-insert into sys_permissions values(1, '/customers/detail','perms[/customers/detail]','获取客户信息');
-insert into sys_permissions values(2, '/customers/findById','perms[/customers/findById]','按id获取用户信息');
-insert into sys_permissions values(3, '/customers/insert','perms[/customers/insert]','插入客户信息');
-insert into sys_permissions values(4, '/customers/update','perms[/customers/update]','获取客户信息');
-insert into sys_permissions values(5, '/customers/delete','perms[/customers/delete]','删除客户信息');
+insert into sys_permissions values(1, '/customers/detail.do','perms[/customers/detail.do]','获取客户信息');
+insert into sys_permissions values(2, '/customers/findById.do','perms[/customers/findById.do]','按id获取用户信息');
+insert into sys_permissions values(3, '/customers/insert.do','perms[/customers/insert.do]','插入客户信息');
+insert into sys_permissions values(4, '/customers/update.do','perms[/customers/update.do]','获取客户信息');
+insert into sys_permissions values(5, '/customers/delete.do','perms[/customers/delete.do]','删除客户信息');
 
-insert into sys_permissions values(6, '/employees/detail','perms[/employees/detail]','获取员工信息');
-insert into sys_permissions values(7, '/employees/findById','perms[/employees/findById]','按id获取员工信息');
-insert into sys_permissions values(8, '/employees/insert','perms[/employees/insert]','插入员工信息');
-insert into sys_permissions values(9, '/employees/update','perms[/employees/update]','获取员工信息');
-insert into sys_permissions values(10, '/employees/delete','perms[/employees/delete]','删除员工信息');
+insert into sys_permissions values(6, '/employees/detail.do','perms[/employees/detail.do]','获取员工信息');
+insert into sys_permissions values(7, '/employees/findById.do','perms[/employees/findById.do]','按id获取员工信息');
+insert into sys_permissions values(8, '/employees/insert.do','perms[/employees/insert.do]','插入员工信息');
+insert into sys_permissions values(9, '/employees/update.do','perms[/employees/update.do]','获取员工信息');
+insert into sys_permissions values(10, '/employees/delete.do','perms[/employees/delete.do]','删除员工信息');
 
-insert into sys_permissions values(11, '/logs/detail','perms[/logs/detail]','获取日志信息');
-insert into sys_permissions values(12, '/logs/findById','perms[/logs/findById]','按id获取日志信息');
-insert into sys_permissions values(13, '/logs/insert','perms[/logs/insert]','插入日志信息');
-insert into sys_permissions values(14, '/logs/update','perms[/logs/update]','获取日志信息');
-insert into sys_permissions values(15, '/logs/delete','perms[/logs/delete]','删除日志信息');
+insert into sys_permissions values(11, '/logs/detail.do','perms[/logs/detail.do]','获取日志信息');
+insert into sys_permissions values(12, '/logs/findById.do','perms[/logs/findById.do]','按id获取日志信息');
+insert into sys_permissions values(13, '/logs/insert.do','perms[/logs/insert.do]','插入日志信息');
+insert into sys_permissions values(14, '/logs/update.do','perms[/logs/update.do]','获取日志信息');
+insert into sys_permissions values(15, '/logs/delete.do','perms[/logs/delete.do]','删除日志信息');
 
-insert into sys_permissions values(16, '/products/detail','perms[/products/detail]','获取商品信息');
-insert into sys_permissions values(17, '/products/findById','perms[/products/findById]','按id获取商品信息');
-insert into sys_permissions values(18, '/products/insert','perms[/products/insert]','插入商品信息');
-insert into sys_permissions values(19, '/products/update','perms[/products/update]','获取商品信息');
-insert into sys_permissions values(20, '/products/delete','perms[/products/delete]','删除商品信息');
+insert into sys_permissions values(16, '/products/detail.do','perms[/products/detail.do]','获取商品信息');
+insert into sys_permissions values(17, '/products/findById.do','perms[/products/findById.do]','按id获取商品信息');
+insert into sys_permissions values(18, '/products/insert.do','perms[/products/insert.do]','插入商品信息');
+insert into sys_permissions values(19, '/products/update.do','perms[/products/update.do]','获取商品信息');
+insert into sys_permissions values(20, '/products/delete.do','perms[/products/delete.do]','删除商品信息');
 
-insert into sys_permissions values(21, '/purchases/detail','perms[/purchases/detail]','获取订单信息');
-insert into sys_permissions values(22, '/purchases/findById','perms[/purchases/findById]','按id获取订单信息');
-insert into sys_permissions values(23, '/purchases/insert','perms[/purchases/insert]','插入订单信息');
-insert into sys_permissions values(24, '/purchases/update','perms[/purchases/update]','获取订单信息');
-insert into sys_permissions values(25, '/purchases/delete','perms[/purchases/delete]','删除订单信息');
+insert into sys_permissions values(21, '/purchases/detail.do','perms[/purchases/detail.do]','获取订单信息');
+insert into sys_permissions values(22, '/purchases/findById.do','perms[/purchases/findById.do]','按id获取订单信息');
+insert into sys_permissions values(23, '/purchases/insert.do','perms[/purchases/insert.do]','插入订单信息');
+insert into sys_permissions values(24, '/purchases/update.do','perms[/purchases/update.do]','获取订单信息');
+insert into sys_permissions values(25, '/purchases/delete.do','perms[/purchases/delete.do]','删除订单信息');
 
-insert into sys_permissions values(26, '/suppliers/detail','perms[/suppliers/detail]','获取供应商信息');
-insert into sys_permissions values(27, '/suppliers/findById','perms[/suppliers/findById]','按id获取供应商信息');
-insert into sys_permissions values(28, '/suppliers/insert','perms[/suppliers/insert]','插入供应商信息');
-insert into sys_permissions values(29, '/suppliers/update','perms[/suppliers/update]','获取供应商信息');
-insert into sys_permissions values(30, '/suppliers/delete','perms[/suppliers/delete]','删除供应商信息');
+insert into sys_permissions values(26, '/suppliers/detail.do','perms[/suppliers/detail.do]','获取供应商信息');
+insert into sys_permissions values(27, '/suppliers/findById.do','perms[/suppliers/findById.do]','按id获取供应商信息');
+insert into sys_permissions values(28, '/suppliers/insert.do','perms[/suppliers/insert.do]','插入供应商信息');
+insert into sys_permissions values(29, '/suppliers/update.do','perms[/suppliers/update.do]','获取供应商信息');
+insert into sys_permissions values(30, '/suppliers/delete.do','perms[/suppliers/delete.do]','删除供应商信息');
 
 insert into sys_roles_permissions values (1,1,1);
 insert into sys_roles_permissions values (2,1,2);
