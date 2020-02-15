d8f385e3713ae8626800e80252a7f7bdf3b45c77
ZhuoZhuoCrayon <873217631@qq.com>
2020-02-15 23:58:10 +0800

    Java编程思想源码-可在idea直接打开

---
+++ TIJ4/code/src/generics/NonCovariantGenerics.java
@@ -0,0 +1,9 @@
+//: generics/NonCovariantGenerics.java
+package generics; /* Added by Eclipse.py */
+// {CompileTimeError} (Won't compile)
+import java.util.*;
+
+public class NonCovariantGenerics {
+  // Compile Error: incompatible types:
+//  List<Fruit> flist = new ArrayList<Apple>();
+} ///:~
