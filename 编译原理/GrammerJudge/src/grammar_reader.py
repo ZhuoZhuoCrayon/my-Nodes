7415a964b3ad6c432ef7b28cfe6ccb85d8ef88ee
ZhuoZhuoCrayon <873217631@qq.com>
2020-04-06 16:03:38 +0800

    稳定更新： [产生式采用元组+列表存储，解决字典无法存重复key的缺点] [读取TYPE信息用于比对]

--- 编译原理/GrammerJudge/src/grammar_reader.py
+++ 编译原理/GrammerJudge/src/grammar_reader.py
@@ -6,7 +6,7 @@
 
 """
 Grammar.txt文件说明
-文法之间以`--------`（八个-号）分隔
+文法之间以`--------TYPE`（八个-号）分隔
 文法行顺序满足 G = {V, T, P, S} ，即文法结构-四行结构
 V：非终结符集
 T：终结符集
@@ -29,24 +29,28 @@ def read_grammars_from_txt():
     for begin in range(0, length, each_grammar_size):
         # print(txt_content_list[begin:begin + each_grammar_size])
         grammar = {}
-        untreated_grammar = txt_content_list[begin:begin + each_grammar_size - 1]
+        untreated_grammar = txt_content_list[begin:begin + each_grammar_size]
         grammar['V'] = untreated_grammar[0].split(',')
         grammar['T'] = untreated_grammar[1].split(',')
         grammar['S'] = untreated_grammar[3]
 
-        # 产生式解析封装：字典类型
-        generative_formulas = {}
+        # 产生式解析封装：
+        # 考虑产生式左部相等情况，以元组列表存储产生式
+        generative_formulas = []
         untreated_generative_formulas = untreated_grammar[2].split(',')
         for untreated_generative_formula in untreated_generative_formulas:
             formula_from, formula_to = untreated_generative_formula.split('->')
             # print(formula_from, formula_to)
-            generative_formulas[formula_from] = formula_to
+            # 此时formula_to格式可能是A|B|C形式，再进行一次切割
+            # 录入：S->A,S->B,S->C
+            for to in formula_to.split('|'):
+                generative_formulas.append((formula_from, to))
         grammar['P'] = generative_formulas
-
+        grammar['TYPE'] = untreated_grammar[-1][8:].split(',')
         # print(grammar)
         grammar_list.append(grammar)
-    for grammar in grammar_list:
-        print(grammar)
+    # for grammar in grammar_list:
+    #     print(grammar)
     return grammar_list
 
 
