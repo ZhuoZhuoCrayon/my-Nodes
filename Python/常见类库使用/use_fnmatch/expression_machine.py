e49f0a62464f24d90c9d571a74f4697648c3d267
crayon <873217631@qq.com>
2020-11-17 23:46:39 +0800

    feature: 范围表达式解析框架

--- Python/常见类库使用/use_fnmatch/expression_machine.py
+++ Python/常见类库使用/use_fnmatch/expression_machine.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
 
+
+class MatchType:
+    WORD = 0
+    RANGE = 1
+    WORD_LIST = 2
+
+
 builtin_char_list = ["*", "[", "]", "?", "!"]
+
+
+def validate_range_scope(begin: str, end: str):
+    if all([begin.isdecimal(), end.isdecimal(), begin < end]):
+        return True
+    if all([
+        len(begin) == 1, len(end) == 1, begin.isalpha(), end.isalpha(), ord(begin) < ord(end)
+    ]):
+        return True
+    raise Exception("表达式解析错误")
 
 
 def replace_builtin_enum_char():
     # 设置规则，对于内置符号，必须
     pass
 
-def parse_expression_part(expression_part: str):
+
+def get_eum_expressions(expression_part: str):
     # 解析[.....]
 
     enum_str_list = []
@@ -28,6 +46,36 @@ def parse_expression_part(expression_part: str):
     return enum_str_list
 
 
+def get_match_type(sub_expression):
+    if "," in sub_expression:
+        return MatchType.WORD_LIST
+    elif "-" in sub_expression:
+        return MatchType.RANGE
+    else:
+        return MatchType.WORD
+
+
+def parse_enum_expression(enum_expression):
+    match_type = get_match_type(enum_expression)
+    return
+
+
+def parse_word_list_expression(word_list_expression: str):
+    elements = word_list_expression.split(",")
+    return [element.strip() for element in elements]
+
+
+def parse_range_expression(range_expression: str):
+    # TODO 0-9a-z类似这种的识别
+    range_list = range_expression.split("-")
+    if len(range_list) != 2:
+        raise Exception("表达式解析错误")
+    begin, end = range_list[0], range_list[1]
+    # 验证范围表达式的有效性
+    validate_range_scope(begin, end)
+    return [chr(_ascii) for _ascii in range(ord(begin), ord(end) + 1)]
+
+
 if __name__ == '__main__':
     test_expression_parts = [
         # 正常的输入
@@ -39,4 +87,4 @@ if __name__ == '__main__':
     ]
 
     for test_expression_part in test_expression_parts:
-        print(test_expression_part, f"\nresult:{parse_expression_part(test_expression_part)}")
+        print(test_expression_part, f"\nresult:{get_eum_expressions(test_expression_part)}")
