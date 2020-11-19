1c8c5ba979103d54311a39f7ae384865c91dab7d
crayon <873217631@qq.com>
2020-11-19 23:57:37 +0800

    feature: 解析枚举合并

--- Python/常见类库使用/use_fnmatch/expression_machine.py
+++ Python/常见类库使用/use_fnmatch/expression_machine.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+import csv
 
 
 class MatchType:
@@ -9,13 +11,31 @@ class MatchType:
 
 builtin_char_list = ["*", "[", "]", "?", "!"]
 
+parse_result_writer = open("parse_result.txt", "w")
 
-def validate_range_scope(begin: str, end: str):
-    if all([begin.isdecimal(), end.isdecimal(), begin < end]):
+
+def list_enum_exp_parse_value(enum_exp_parse_list):
+    if isinstance(enum_exp_parse_list, str):
+        return [enum_exp_parse_list]
+    enum_value_list = []
+    for value_exp in enum_exp_parse_list:
+        if isinstance(value_exp, str):
+            enum_value_list.append(value_exp)
+        else:
+            enum_value_list.extend(value_exp)
+    return enum_value_list
+
+
+def validate_range_scope(range_expression):
+    range_list = range_expression.split("-")
+    if len(range_list) != 2:
+        return False
+    begin, end = range_list[0], range_list[1]
+    if begin.isdecimal() and end.isdecimal() and int(begin) < int(end):
         return True
-    if all([len(begin) == 1, len(end) == 1, begin.isalpha(), end.isalpha(), ord(begin) < ord(end)]):
+    if len(begin) == 1 and len(end) == 1 and begin.isalpha() and end.isalpha() and ord(begin) < ord(end):
         return True
-    raise Exception("表达式解析错误")
+    return False
 
 
 def replace_builtin_enum_char():
@@ -27,6 +47,7 @@ def get_eum_expressions(expression_part: str):
     # 解析[.....]
 
     enum_str_list = []
+    last_enum_end = -1
 
     enum_begin = expression_part.find("[")
     while enum_begin != -1:
@@ -36,18 +57,23 @@ def get_eum_expressions(expression_part: str):
             raise Exception("表达式解析错误")
 
         enum_expression = expression_part[enum_begin: enum_end + 1]
-
+        enum_value_list = list_enum_exp_parse_value(parse_enum_expression(enum_expression[1: -1], 4))
+        print(enum_value_list)
         enum_str_list.append(enum_expression)
 
+        if last_enum_end + 1 != enum_begin:
+            print(expression_part[last_enum_end + 1: enum_begin])
+        last_enum_end = enum_end
         enum_begin = expression_part.find("[", enum_end)
-
-    return [parse_enum_expression(enum_str[1:-1], 4) for enum_str in enum_str_list]
 
 
 def get_match_type(sub_expression):
     if "," in sub_expression:
         return MatchType.WORD_LIST
     elif "-" in sub_expression:
+        # TODO: 用[[]来屏蔽内置符比较合理
+        if not validate_range_scope(sub_expression):
+            return MatchType.WORD
         return MatchType.RANGE
     else:
         return MatchType.WORD
@@ -65,7 +91,7 @@ def parse_enum_expression(enum_expression, indent):
     sub_expressions = match_status_func[match_type](enum_expression)
     if match_type == MatchType.WORD:
         return match_status_func[match_type](enum_expression)
-    print(f"{' ' * indent}{sub_expressions}")
+    parse_result_writer.write(f"{' ' * indent}{sub_expressions}\n")
     return [parse_enum_expression(sub_expression, indent + 4) for sub_expression in sub_expressions]
 
 
@@ -81,8 +107,15 @@ def parse_range_expression(range_expression: str):
         raise Exception("表达式解析错误")
     begin, end = range_list[0], range_list[1]
     # 验证范围表达式的有效性
-    validate_range_scope(begin, end)
-    return [chr(_ascii) for _ascii in range(ord(begin), ord(end) + 1)]
+    if begin.isdecimal() and end.isdecimal() and int(begin) < int(end):
+        return [str(number) for number in range(int(begin), int(end) + 1)]
+    if len(begin) == 1 and len(end) == 1 and begin.isalpha() and end.isalpha() and ord(begin) < ord(end):
+        return [chr(_ascii) for _ascii in range(ord(begin), ord(end) + 1)]
+    raise Exception("表达式解析错误")
+
+
+def mock_sops_var(expression):
+    return re.sub(r"[$]?{.*?}", "<MOCK_SOPS_VAR>", expression)
 
 
 if __name__ == "__main__":
@@ -90,13 +123,20 @@ if __name__ == "__main__":
         # 正常的输入
         "[1, 2, 3].[txt, tar]",
         "file[1-3].txt",
-        "[python, java][3.6.8, 10.3.2].exe[1-3]",
-        # 异常，比如嵌套
-        "[a, b[ab, cd], e]",
-        "[1-4, 6-8, a-z]",
+        "cxx[python, java]dssf[3.6.8, 10.3.2].exe[1-3, a-z]",
+        "aa[b-c, 0-9]aa",
+        "[1-4, 6-8, a-z][a]",
+        "[aaaaaaaa]"
     ]
 
+    csv_file = open("expression.csv", "r")
+    csv_file_reader = csv.reader(csv_file)
+
+    test_expression_parts.extend(
+        [mock_sops_var(line[0]) for line in csv_file_reader if line]
+    )
+
     for test_expression_part in test_expression_parts:
-        print(f"parse <{test_expression_part}> ...")
+        parse_result_writer.write(f"parse <{test_expression_part}> ...\n")
         get_eum_expressions(test_expression_part)
-        print("---------------------------------------------------------------------------")
+        parse_result_writer.write("---------------------------------------------------------------------------\n")
