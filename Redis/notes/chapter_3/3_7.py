66682ebb414da539e528353a6e89edf01b0a9570
crayon <873217631@qq.com>
2021-01-10 00:24:36 +0800

    feature: pipeline or no pipeline 性能对比

--- Redis/notes/chapter_3/3_7.py
+++ Redis/notes/chapter_3/3_7.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import time
-from concurrent.futures import ThreadPoolExecutor
+import random
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from functools import wraps
 
 from Redis.redisbase.config.default import REDIS_KEY_PREFIX
 from Redis.redisbase.conn import RedisInstSingleTon
@@ -64,6 +66,100 @@ def trans_main() -> None:
     # [2]incr_result: 1
 
 
+# pipeline for test
+def pipeline_range():
+    hash_members_key = f"{REDIS_KEY_PREFIX}hash:member:"
+    pipeline = REDIS_INST.pipeline()
+    for index in range(1, 10):
+        pipeline.hset(name=hash_members_key, key=f"member-{index}", value=index)
+        pipeline.delete(*[f"{REDIS_KEY_PREFIX}member:{index}"])
+        pipeline.set(name=f"{REDIS_KEY_PREFIX}member:{index}", value=index)
+        pipeline.expire(name=f"{REDIS_KEY_PREFIX}member:{index}", time=300)
+    pipeline.execute()
+
+
+# compare pipeline and not pipeline
+
+def program_timer(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        cost_time = time.time() - start_time
+        print(f"{func.__name__} cost time: {cost_time}.\n")
+        return result
+
+    return inner
+
+
+def op_without_pipeline(slice_size: int, slice_index: int) -> None:
+    """
+    pipeline page - 20
+    1000  -  11.3s
+    5000  -  52.4s
+    10000 -  ...
+
+    pipeline page - 5
+    1000  -  3s
+    5000  -  17s
+    10000 -  30s
+    """
+    large_pkg_str = "|".join([str(num) for num in range(1, 10)])
+    for index in range(slice_size * (slice_index - 1), slice_size * slice_index):
+        REDIS_INST.hset(
+            name=f"{REDIS_KEY_PREFIX}hash:without_pipeline:{random.randint(0, 5)}",
+            key=f"index-{index}",
+            value=f"{large_pkg_str}-{index}"
+        )
+
+
+def op_with_pipeline(slice_size: int, slice_index: int) -> None:
+    """
+    1000  -  1.2s
+    5000  -  4.5s
+    10000 -  11.4s
+
+    pipeline page - 5
+    1000  -  1s
+    5000  -  6s
+    10000 -  11.6s
+    """
+    large_pkg_str = "|".join([str(num) for num in range(1, 10)])
+    pipeline = REDIS_INST.pipeline()
+    for index in range(slice_size * (slice_index - 1), slice_size * slice_index):
+        pipeline.hset(
+            name=f"{REDIS_KEY_PREFIX}hash:with_pipeline:{random.randint(0, 5)}",
+            key=f"index-{index}",
+            value=f"{large_pkg_str}-{index}"
+        )
+    pipeline.execute()
+    # print(f"[{slice_index}]op_with_pipeline finished.\n")
+
+
+@program_timer
+def pipeline_perform_test(mode: str):
+
+    cases = {
+        "with": op_with_pipeline,
+        "without": op_without_pipeline
+    }
+
+    hash_key_used = []
+    for index in range(0, 5 + 1):
+        hash_key_used.extend([
+            *[f"{REDIS_KEY_PREFIX}hash:with_pipeline:{index}", f"{REDIS_KEY_PREFIX}hash:without_pipeline:{index}"]
+        ])
+    REDIS_INST.delete(*hash_key_used)
+
+    with ThreadPoolExecutor(max_workers=100) as ex:
+        tasks = [ex.submit(cases[mode], slice_index=slice_index, slice_size=20) for slice_index in range(1, 1000)]
+
+    for future in as_completed(tasks):
+        future.result()
+
+
 if __name__ == '__main__':
-    no_trans_main()
-    trans_main()
+    # no_trans_main()
+    # trans_main()
+    # pipeline_range()
+    pipeline_perform_test("with")
